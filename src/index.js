dfe2a7e7481c062b4df16b2ef97430d709b8b0b1
Subir <subirgupta090@gmail.com>
2023-03-07 21:52:34 +0530

    First commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { PORT } = require('./config/serverConfig');
+
+
+function setUpAndStartServer() {
+    const app = express();
+    
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended : true}));
+
+    app.listen(PORT, () => {
+        console.log(`Server started at ${PORT}`);
+    });
+}
+
+setUpAndStartServer();
